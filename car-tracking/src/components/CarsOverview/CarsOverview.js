4bf913af90c8c6af257d053956fd6c1f23d223ba
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-03-04 13:34:43 +0200

    Add CarsOverview Component

---
+++ car-tracking/src/components/CarsOverview/CarsOverview.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import classes from "./CarsOverview.module.css";
+
+import Card from "../UI/Card";
+import SearchBox from "../UI/SearchBox";
+import VerticalStepper from "../UI/VerticalStepper";
+import CarIcon from "../UI/CarIcon";
+
+const CarsOverview = () => {
+	const steps = [
+		{ icon: <CarIcon />, label: "abc123", description: "Was not updated" },
+		{
+			icon: <CarIcon />,
+			label: "xyz987",
+			description: "Last updated 9 minutes ago",
+		},
+	];
+
+	const [query, setQuery] = useState("");
+
+	const onSearchHandler = (query) => {
+		setQuery(query);
+	};
+
+	const availableCars = steps.filter((car) => {
+		if (query === "") {
+			return car;
+		} else if (car.label.toLowerCase().includes(query.toLowerCase())) {
+			return car;
+		}
+	});
+
+	return (
+		<Card className={classes.overview}>
+			<h4>Cars Overview</h4>
+			<SearchBox onSearch={onSearchHandler} />
+			<VerticalStepper steps={availableCars} />
+		</Card>
+	);
+};
+
+export default CarsOverview;
