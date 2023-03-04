22fb0254dd746c7ab86488402bd680b27dc76c2a
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-03-04 17:04:26 +0200

    Update App Component to Newly Add Components

--- car-tracking/src/App.js
+++ car-tracking/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Map from "./components/Map/Map";
+
+import CarsOverview from "./components/CarsOverview/CarsOverview";
+import AvgSpeedChart from "./components/Chart/AvgSpeedChart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<div className="grid-container">
+				<div className="map__container">
+					<Map />
+				</div>
+				<div className="overview__container">
+					<CarsOverview />
+				</div>
+				<div className="chart__container">
+					<AvgSpeedChart />
+				</div>
+			</div>
+		</main>
+	);
 }
-
 export default App;
