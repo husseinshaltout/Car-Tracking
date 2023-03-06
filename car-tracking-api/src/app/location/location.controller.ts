b661c639e91c003a81cd75948f9ba0a49b5b9ba1
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-03-06 18:26:25 +0200

    Add Location Controller

---
+++ car-tracking-api/src/app/location/location.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction, Router } from 'express';
+
+import catchAsync from '@common/middlewares/catchAsync';
+
+import HttpStatus from '@common/enums/httpStatus';
+
+import locationService from '@app/location/location.service';
+
+class LocationController {
+  public router = Router();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    // TODO: Add body Validation
+    this.router.get('/', catchAsync(this.getAllLocations));
+    this.router.patch('/:plateNumber', catchAsync(this.updateCarLocation));
+  }
+
+  private async getAllLocations(req: Request, res: Response) {
+    const locations = await locationService.getAllLocations();
+    res.status(HttpStatus.OK).json({
+      message: 'All Locations retrieved successfully',
+      locations: locations,
+    });
+  }
+
+  private async updateCarLocation(req: Request, res: Response) {
+    const { plateNumber } = req.params;
+    const car = await locationService.updateCarLocation(plateNumber, req.body);
+
+    res.status(HttpStatus.OK).json({
+      message: 'Car coordinates updated successfully',
+      car: car,
+    });
+  }
+}
+
+const locationController = new LocationController();
+export default locationController;
